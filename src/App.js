6009ba01c2d93a39732b0207409e6a84b40bfbd7
Payal Thummar <payalthummar1994@gmail.com>
2022-11-25 10:02:12 +0100

    project starte

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const url =
+    "https://geo.ipify.org/api/v2/country?apiKey=at_007uK5CyJKclEY1wU5Sle8KIdwYOa&ipAddress=8.8.8.8";
+
+  function fetchData() {
+    axios
+      .get(url)
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
